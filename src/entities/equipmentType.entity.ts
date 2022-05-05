3ac5c4bea3128e5aa5f587772d0f289a3fb3b892
Julien Goubet <goubet.julien35@gmail.com>
2022-05-05 23:46:15 +0200

    added some icons

--- src/entities/equipmentType.entity.ts
+++ src/entities/equipmentType.entity.ts
@@ -3,11 +3,15 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('Equipment_Type')
 export default class EquipmentType {
-  @ApiProperty()
+  @ApiProperty({ type: () => String })
   @PrimaryColumn({ type: 'varchar', length: 10 })
   code!: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: () => String })
   @Column({ type: 'varchar', length: 60 })
   label!: string;
+
+  @ApiProperty({ type: () => String })
+  @Column({ type: 'varchar', length: 10 })
+  icon!: string;
 }
