6725bbfd53e492ef5317e44604401418b2a5d173
Julien <goubet.julien35@gmail.com>
2022-06-16 16:32:15 +0200

    role guard

--- src/auth/guards/roles.guards.ts
+++ src/auth/guards/roles.guards.ts
@@ -18,6 +18,7 @@ export class RolesGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest();
     const user = request.user as JwtPayload;
+    if (!user.role) return false;
     return requiredRoles.includes(user.role);
   }
 }
